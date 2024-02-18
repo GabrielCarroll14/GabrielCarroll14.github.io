91f6c52411c089299470ed2f153a8cb3cb41a9cb
Gabriel Carroll <gabrielcarroll11@gmail.com>
2024-02-18 17:20:29 +0000

    Add as section to index.html containing this sites stars on GitHub

---
+++ stars.js
@@ -0,0 +1,19 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    const repoOwner = 'gabrielcarroll14';
+    const repoName = 'GabrielCarroll14.github.io';
+
+    fetch(`https://api.github.com/repos/${repoOwner}/${repoName}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log('GitHub API Response:', data);
+
+            const starsCount = data.stargazers_count;
+            if (starsCount !== undefined) {
+                document.getElementById('starsCount').innerText = `Stars: ${starsCount}`;
+            } else {
+                console.error('Stars count not found in the GitHub API response.');
+            }
+        })
+        .catch(error => console.error('Error fetching data:', error));
+});
