c6855d7b23385adc363450a3695f16f4ab912b36
GabrielCarroll <gabrielcarroll11@gmail.com>
2024-03-18 12:03:43 +0000

    Idk why this isnt working?

--- script.js
+++ script.js
@@ -8,25 +8,27 @@ function changecolour(color) {
     var header = document.getElementById("header");
     var name = document.getElementById("name");
     var navbar = document.getElementById("navbar");
-    var button = document.getElementById("themeButtons");
+    var button = document.getElementById("buttons");
     
     // If the colour that was passed in when the function was called is black change all the appropriate colours
     if (color == 'black') {
         document.body.style.backgroundColor = 'black';
-        status.style.color = 'white';
+        status.style.backgroundColor = 'white';
         description.style.color = 'white';
         header.style.color = 'white';
         name.style.color = 'white';
         navbar.style.color = 'white';
+        button.style.backgroundColor = 'white';
     }
     
     // If the passed in colour wasn't black change all appropriate colours
     else {
         document.body.style.backgroundColor = 'white';
-        status.style.color = 'black';
+        status.style.backgroundColor = 'black';
         description.style.color = 'black';
         header.style.color = 'black';
         name.style.color = 'black';
         navbar.style.color = 'black';
+        button.style.backgroundColor = 'black';
     }
 }
