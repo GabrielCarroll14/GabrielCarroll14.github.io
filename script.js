bfb09b6787883012c4d422a5d464877d795fe826
Gabriel Carroll <gabrielcarroll11@gmail.com>
2024-03-18 10:15:01 +0000

    MORE COMMENTS!!!!!!!!!

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
+
+// Get the color that is passed in when the function is called and create the function
 function changecolour(color) {
+    
+    // Get all the id's of the elements that will change colour and put the in variables
     var status = document.getElementById("status");
     var description = document.getElementById("description");
     var header = document.getElementById("header");
     var name = document.getElementById("name");
     var navbar = document.getElementById("navbar");
+    
+    // If the colour that was passed in when the function was called is black change all the appropriate colours
     if (color == 'black') {
         document.body.style.backgroundColor = 'black';
         status.style.color = 'white';
@@ -13,7 +19,7 @@ function changecolour(color) {
         navbar.style.color = 'white';
     }
     
-    
+    // If the passed in colour wasn't black change all appropriate colours
     else {
         document.body.style.backgroundColor = 'white';
         status.style.color = 'black';
